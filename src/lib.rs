d2988a6db6b2a8e4df5c2aaf1107fb94a265536b
NathanielSong <ampnattahapat@gmail.com>
2025-06-05 16:03:07 +0700

    todo fix bridge

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ pub fn hash_sha256(input: &str) -> String {
     let mut hasher = Sha3_256::new();
     hasher.update(input);
     let result = hasher.finalize();
-    result.iter().map(|b| format!("{:02x}", b)).collect()
+    return result.iter().map(|b| format!("{:02x}", b)).collect();
 }
