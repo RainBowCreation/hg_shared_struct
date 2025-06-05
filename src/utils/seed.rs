18f6373c8709405b8cb894b92acb6ad1c361a00e
NathanielSong <ampnattahapat@gmail.com>
2025-06-05 11:17:16 +0700

    hotfix

--- src/utils/seed.rs
+++ src/utils/seed.rs
@@ -1,7 +1,7 @@
-use sha3::{Sha256, Digest};
+use sha3::{Sha3_256, Digest};
 
 pub fn hash_sha256(input: &str) -> String {
-    let mut hasher = Sha256::new();
+    let mut hasher = Sha3_256::new();
     hasher.update(input.as_bytes());
     let result = hasher.finalize();
     hex::encode(result)
