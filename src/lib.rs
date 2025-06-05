5ff60d22c8745a9ee26610109404d7f364c59d43
NathanielSong <ampnattahapat@gmail.com>
2025-06-05 13:37:56 +0700

    hotfix

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
-mod utils;
-use utils::seed::hash_sha256;
+use sha3::{Sha3_256, Digest};
+
+pub fn hash_sha256(input: &str) -> String {
+    let mut hasher = Sha3_256::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+    result.iter().map(|b| format!("{:02x}", b)).collect()
+}
