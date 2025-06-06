4867a96b46687980eacc94d608c2c96cad7cc836
NathanielSong <ampnattahapat@gmail.com>
2025-06-06 09:30:43 +0700

    move StUuid implments

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,56 @@
 use specta::{Type, ts};
 use sha3::{Sha3_256, Digest};
-use uuid::Uuid;
-use serde::{Serialize, Deserialize};
+use serde::{Serialize, Deserialize as SerdeDeserialize};
 use time::Time;
 
-#[derive(Debug, Serialize, Deserialize, Type)]
+use uuid::Uuid;
+use std::{fmt::Display, ops::Deref};
+use spacetimedb::{
+    sats::{impl_deserialize, impl_serialize, impl_st},
+    ReducerContext,
+};
+
+
+// st_uuid implementation
+#[derive(Debug, Serialize, SerdeDeserialize, Type)]
 pub struct StUuid(pub Uuid);
+
+// spacetimedb impls
+impl_st!([] StUuid, spacetimedb::sats::AlgebraicType::String);
+impl_serialize!([] StUuid, (self, ser) => {
+    ser.serialize_str(self.hyphenated().encode_upper(&mut Uuid::encode_buffer()))
+});
+impl_deserialize!([] StUuid, de => {
+    let s = <std::string::String as spacetimedb::Deserialize>::deserialize(de).map(|s| s.into_boxed_str())?;
+    Ok(Uuid::parse_str(&s).map(|u| u.into()).expect("Failed to Deserialize to UUID"))
+});
+
+impl StUuid {
+    pub fn new(ctx: &ReducerContext) -> Self {
+        StUuid(Uuid::from_bytes(ctx.random()))
+    }
+}
+
+impl Display for StUuid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0.to_string())
+    }
+}
+
+impl Deref for StUuid {
+    type Target = Uuid;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<Uuid> for StUuid {
+    fn from(val: Uuid) -> Self {
+        Self(val)
+    }
+}
+
+// Seed and related types
 
 #[derive(Type)]
 pub struct Seed {
